2ef8a230c8d9a81d021e520f818fd8e55b679cbf
ShakaUVM </u/ShakaUVM>
2020-08-30 00:25:57 -0700

    Added header guard

--- read.h
+++ read.h
@@ -1,11 +1,11 @@
 //read.h - a tiny header-only library to allow input in C++ to be more sensible
 //by ShakaUVM
+#pragma once
 #include <iostream>
 #include <fstream>
 #include <string>
 
-//Simplest Version, Designed for Newbies
-//This will read a T from cin, reprompting if they type in something wrong
+//Read a T from cin, reprompting if they type in something wrong
 //Examples:
 //int x = read<int>();
 //const int x = read<int>("Please enter an int");
@@ -28,7 +28,7 @@ T read(const std::string prompt = "") {
 	}
 }
 
-//This version reads from a file, so no prompt
+//Read a T from a file, so no prompt
 //Example:
 //auto d = read<double>(file);
 template<class T>
@@ -48,7 +48,7 @@ T read(std::istream &ins) {
 	}
 }
 
-//To read a whole line of text, analogue to getline
+//Reads a whole line of text, analogue to getline
 std::string readline(const std::string prompt = "") {
 	//Eliminate a common bug when switching from >> to getline, the >> will leave a newline in the input buffer
 	std::string retval;
@@ -62,7 +62,7 @@ std::string readline(const std::string prompt = "") {
 	return retval;
 }
 
-//Readline for reading from a file
+//Getline equivalent for reading from a file
 std::string readline(std::istream &ins) {
 	std::string retval;
 	ins >> std::ws;
